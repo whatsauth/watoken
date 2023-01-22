280c400fba35ae9f4b6c41e61962f32e85a3e5d5
Rolly Maulana Awangga <rolly@awang.ga>
2023-01-23 04:32:33 +0700

    encode for secon hour minutes

--- watoken.go
+++ watoken.go
@@ -33,14 +33,52 @@ func Encode(id string, privateKey string) (string, error) {
 
 }
 
-func Decode(publicKey string, signed string) (payload Payload, err error) {
+func EncodeforHours(id string, privateKey string, hours int32) (string, error) {
+	token := paseto.NewToken()
+	token.SetIssuedAt(time.Now())
+	token.SetNotBefore(time.Now())
+	token.SetExpiration(time.Now().Add(time.Duration(hours) * time.Hour))
+	token.SetString("id", id)
+	secretKey, err := paseto.NewV4AsymmetricSecretKeyFromHex(privateKey)
+	return token.V4Sign(secretKey, nil), err
+
+}
+
+func EncodeforMinutes(id string, privateKey string, minutes int32) (string, error) {
+	token := paseto.NewToken()
+	token.SetIssuedAt(time.Now())
+	token.SetNotBefore(time.Now())
+	token.SetExpiration(time.Now().Add(time.Duration(minutes) * time.Minute))
+	token.SetString("id", id)
+	secretKey, err := paseto.NewV4AsymmetricSecretKeyFromHex(privateKey)
+	return token.V4Sign(secretKey, nil), err
+
+}
+
+func EncodeforSeconds(id string, privateKey string, seconds int32) (string, error) {
+	token := paseto.NewToken()
+	token.SetIssuedAt(time.Now())
+	token.SetNotBefore(time.Now())
+	token.SetExpiration(time.Now().Add(time.Duration(seconds) * time.Second))
+	token.SetString("id", id)
+	secretKey, err := paseto.NewV4AsymmetricSecretKeyFromHex(privateKey)
+	return token.V4Sign(secretKey, nil), err
+
+}
+
+func Decode(publicKey string, tokenstring string) (payload Payload, err error) {
 	var token *paseto.Token
 	var pubKey paseto.V4AsymmetricPublicKey
 	pubKey, err = paseto.NewV4AsymmetricPublicKeyFromHex(publicKey) // this wil fail if given key in an invalid format
-	fmt.Println(err)
-	parser := paseto.NewParser()                           // only used because this example token has expired, use NewParser() (which checks expiry by default)
-	token, err = parser.ParseV4Public(pubKey, signed, nil) // this will fail if parsing failes, cryptographic checks fail, or validation rules fail
-	json.Unmarshal(token.ClaimsJSON(), &payload)
-
+	if err != nil {
+		fmt.Println("Deocode NewV4AsymmetricPublicKeyFromHex : ", err)
+	}
+	parser := paseto.NewParser()                                // only used because this example token has expired, use NewParser() (which checks expiry by default)
+	token, err = parser.ParseV4Public(pubKey, tokenstring, nil) // this will fail if parsing failes, cryptographic checks fail, or validation rules fail
+	if err != nil {
+		fmt.Println("Deocode ParseV4Public : ", err)
+	} else {
+		json.Unmarshal(token.ClaimsJSON(), &payload)
+	}
 	return payload, err
 }
--- watoken_test.go
+++ watoken_test.go
@@ -14,14 +14,40 @@ func TestWatoken(t *testing.T) {
 	fmt.Println("publicKey : ", publicKey)
 	userid := "awangga"
 
-	signed, err := Encode(userid, privateKey)
+	tokenstring, err := Encode(userid, privateKey)
 	require.NoError(t, err)
-	body, err := Decode(publicKey, signed)
-	fmt.Println("signed : ", signed)
+	body, err := Decode(publicKey, tokenstring)
+	fmt.Println("signed : ", tokenstring)
 	fmt.Println("isi : ", body)
 	require.NoError(t, err)
 }
 
+func TestWatokenDuration(t *testing.T) {
+	privateKey, publicKey := GenerateKey()
+	fmt.Println("privateKey : ", privateKey)
+	fmt.Println("publicKey : ", publicKey)
+	userid := "awangga"
+
+	tokenstring, err := EncodeforSeconds(userid, privateKey, 60)
+	require.NoError(t, err)
+	body, err := Decode(publicKey, tokenstring)
+	fmt.Println("tokenstring duration : ", tokenstring)
+	fmt.Println("isi : ", body)
+	require.NoError(t, err)
+}
+
+/*
+	 func TestWaTokenDecodewithStaticKey(t *testing.T) {
+		//privateKey := "461ce0e87748fd656c518b870da217dc200fc8d3b6275dda8cf14943424bf8c49e2ece1954df1ea8b151fba59cc7cbd4fb810b69716149e1c26169227bd5b686"
+		publicKey := "20d9c3fbb219640c9baf0846a28fc6d3971924ccdd0175ec76bef435b248ed83"
+		//userid := "awangga"
+		tokenstring := "v4.public.eyJleHAiOiIyMDIzLTAxLTIzVDA0OjI3OjQzKzA3OjAwIiwiaWF0IjoiMjAyMy0wMS0yM1QwNDoyNjo0MyswNzowMCIsImlkIjoiYXdhbmdnYSIsIm5iZiI6IjIwMjMtMDEtMjNUMDQ6MjY6NDMrMDc6MDAifevjpmzIHYoUu9ZW_B15FPcEVSglaAxmACxyw5UEX4mpPnnIo53DOfUFAspGt2QNG8fwgnAH_volax7ZW4NAZwU"
+		body, err := Decode(publicKey, tokenstring)
+		fmt.Println("TestWaTokenDecodewithStaticKey isi : ", body)
+		require.NoError(t, err)
+
+}
+*/
 func TestWacipher(t *testing.T) {
 	n := 100
 	rnd := RandomString(n)
