47e2b7480bd6621df450265f1617a2d147d1617b
Rolly Maulana Awangga <rolly@awang.ga>
2023-01-29 18:13:05 +0700

    RandomLowerCaseStringwithSpecialCharacter

--- wacipher.go
+++ wacipher.go
@@ -31,6 +31,26 @@ func GetAppSubDomain(wuid string) (subdomain string) {
 	return
 }
 
+func RandomLowerCaseStringwithSpecialCharacter(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	chars := []rune("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-[]{}<>/?|=,.~`")
+	var b strings.Builder
+	for i := 0; i < length; i++ {
+		b.WriteRune(chars[rand.Intn(len(chars))])
+	}
+	return b.String()
+}
+
+func RandomLowerCaseString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	chars := []rune("abcdefghijklmnopqrstuvwxyz0123456789")
+	var b strings.Builder
+	for i := 0; i < length; i++ {
+		b.WriteRune(chars[rand.Intn(len(chars))])
+	}
+	return b.String()
+}
+
 func RandomString(length int) string {
 	rand.Seed(time.Now().UnixNano())
 	chars := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
