ac7fc9ca763228927c2d3af560926fceb3cf097a
Rolly Maulana Awangga <awangga@gmail.com>
2025-07-25 15:23:11 +0700

    update test terbaru

--- watoken_test.go
+++ watoken_test.go
@@ -1,15 +1,30 @@
 package watoken
 
 import (
-	"os"
+	"fmt"
 	"testing"
 )
 
-func TestEncode(t *testing.T) {
+/* func TestEncode(t *testing.T) {
 	privkey := os.Getenv("PRIVATEKEY")
 	str, _ := EncodeforHours(os.Getenv("PHONENUMBER"), "Helpdesk Pamong Desa", privkey, 43830)
 	println(str)
 	//atr, _ := DecodeGetId("", str)
 	//println(atr)
+
+} */
+
+func TestWatoken(t *testing.T) {
+	privateKey, publicKey := GenerateKey()
+
+	//generate token for user awangga
+	userid := "awangga"
+	tokenstring, _ := Encode(userid, privateKey)
+
+	//decode token to get userid
+	useridstring, _ := DecodeGetId(publicKey, tokenstring)
+	if useridstring == "" {
+		fmt.Println("expire token")
+	}
 
 }
